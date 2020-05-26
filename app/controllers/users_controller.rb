0ecb8e69c687abb0e0d84ae871f1d158b91e835c
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-05-26 20:23:17 +0530

    added send friendship request functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,17 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
+    @pending_requests = Frienship.where(friend_id: params[:id])
   end
 
   def friends_check
     return true if Friendship.where(user_id: current_user.id, friend_id: params[:id], confirmed: true)
   end
 
-  def send_friend 
+  def send_friend
+    friendship = Friendship.create(user_id: current_user.id, friend_id: params[:id], confirmed: false)
+
+    redirect_to users_path, notice: 'Friend request sent.'
   end
 
   def accept_friend
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,7 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, :class_name => "User"
+
+  # scope :requests, -> { where(:confirmed => false)}
+  # scope :friends, -> { where(:confirmed => true)}
 end
