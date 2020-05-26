8ce32a64c1f5527e9b5c78da97685af26fb2f6ac
javitocor <javitocor@gmail.com>
2020-05-26 11:35:42 +0200

    Modified routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,4 @@ Rails.application.routes.draw do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
   end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
