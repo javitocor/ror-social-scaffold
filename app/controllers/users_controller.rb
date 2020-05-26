f0285bb57fbe54a5ef4c001f13d6cf41e0512486
javitocor <javitocor@gmail.com>
2020-05-26 15:56:36 +0200

    Added friendship actions to users controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
   end
+
+  def friends_check
+    return true if Friendship.where(user_id: current_user.id, friend_id: params[:id], confirmed: true)
+  end
+
+  def send_friend 
+  end
+
+  def accept_friend
+  end
+
+  def reject_friend 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do 
+    member do 
+      get 'send_friend'
+    end
+  end
+
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
