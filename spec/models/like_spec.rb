0becd967364cc5909a1069e7be7b0821efd72e96
javitocor <javitocor@gmail.com>
2020-05-28 16:54:21 +0200

    Fixed rubocop offenses

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 require 'capybara/rspec'
 
 RSpec.describe Like, type: :model do
-    context 'Likes associations tests' do
-        it { should belong_to(:user) }
-        it { should belong_to(:post) }
-    end
+  context 'Likes associations tests' do
+    it { should belong_to(:user) }
+    it { should belong_to(:post) }
+  end
 end
