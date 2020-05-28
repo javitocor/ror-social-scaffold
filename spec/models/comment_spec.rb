4009d2d5b24863037510616be89fce6b7872ff94
javitocor <javitocor@gmail.com>
2020-05-28 16:54:21 +0200

    Fixed rubocop offenses

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 require 'capybara/rspec'
 
-RSpec.describe Comment, type: :model do 
-    context 'Comment associations tests' do
-        it { should belong_to(:user) }
-        it { should belong_to(:post) }
-    end
+RSpec.describe Comment, type: :model do
+  context 'Comment associations tests' do
+    it { should belong_to(:user) }
+    it { should belong_to(:post) }
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 require 'capybara/rspec'
 
 RSpec.describe Post, type: :model do
-    context 'Post associations tests' do
-        it { should have_many(:comments) }
-        it { should have_many(:likes) }
-    end
+  context 'Post associations tests' do
+    it { should have_many(:comments) }
+    it { should have_many(:likes) }
+  end
 end
