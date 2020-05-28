d8abe430cb1c26e7835283ca14fa648b2425b216
javitocor <javitocor@gmail.com>
2020-05-28 07:11:11 +0200

    Added faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+15.times do
+    User.create!(
+      username: Faker::Artist,
+      email: Faker::Internet.email,
+      password: 'password',
+      password_confirmation: 'password'
+    )
+  end
