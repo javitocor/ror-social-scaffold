816a63a43e8e36394c8ed912bb48a41539e53513
javitocor <javitocor@gmail.com>
2020-05-28 12:33:09 +0200

    Modified users controller tests and added unfriend test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -19,57 +19,79 @@ RSpec.describe UsersController, type: :feature do
       expect(page).to have_content('Name:')
     end
   end
-end
+  context 'Testing send friend request' do 
+    let(:user) { User.create(id: '1', name: 'Peter', email: 'peter@example.com', password: 'password') }
+    before :each do
+      User.create(id: '2', name: 'Mick', email: 'mick@example.com', password: 'password')
+      visit new_user_session_path
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
+      click_button 'Log in'
+    end
 
-RSpec.describe 'Testing send friend request', type: :feature do
-  let(:user) { User.create(id: '1', name: 'Peter', email: 'peter@example.com', password: 'password') }
-  before :each do
-    User.create(id: '2', name: 'Mick', email: 'mick@example.com', password: 'password')
-    visit new_user_session_path
-    fill_in 'user_email', with: user.email
-    fill_in 'user_password', with: user.password
-    click_button 'Log in'
+    scenario 'friend request link' do
+      visit users_path
+      expect(page).to have_content('Send friend request')
+    end
+
+    scenario 'send friend request' do
+      visit users_path
+      click_link 'Send friend request'
+      expect(page).to have_content('Friend request pending')
+    end
   end
+  context 'Testing Accept and Decline friend requests' do 
+    let(:user) { User.create(id: '1', name: 'Peter', email: 'peter@example.com', password: 'password') }
+    before :each do
+      user2 = User.create(id: '2', name: 'Mick', email: 'mick@example.com', password: 'password')
+      visit new_user_session_path
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
+      click_button 'Log in'
+      visit users_path
+      click_link 'Send friend request'
+      click_link 'Sign out'
+      visit new_user_session_path
+      fill_in 'user_email', with: user2.email
+      fill_in 'user_password', with: user2.password
+      click_button 'Log in'
+      visit user_path(user2)
+    end
 
-  scenario 'friend request link' do
-    visit users_path
-    expect(page).to have_content('Send friend request')
+    scenario 'accept friend request' do
+      click_link 'Accept'
+      visit users_path
+      expect(page).to have_content('Friends!!!')
+    end
+
+    scenario 'decline friend request' do
+      click_link 'Decline'
+      visit users_path
+      expect(page).to have_content('Send friend request')
+    end
   end
-
-  scenario 'send friend request' do
-    visit users_path
-    click_link 'Send friend request'
-    expect(page).to have_content('Friend request pending')
-  end
-end
-
-RSpec.describe 'Testing Accept and Decline friend requests', type: :feature do
-  let(:user) { User.create(id: '1', name: 'Peter', email: 'peter@example.com', password: 'password') }
-  before :each do
-    user2 = User.create(id: '2', name: 'Mick', email: 'mick@example.com', password: 'password')
-    visit new_user_session_path
-    fill_in 'user_email', with: user.email
-    fill_in 'user_password', with: user.password
-    click_button 'Log in'
-    visit users_path
-    click_link 'Send friend request'
-    click_link 'Sign out'
-    visit new_user_session_path
-    fill_in 'user_email', with: user2.email
-    fill_in 'user_password', with: user2.password
-    click_button 'Log in'
-    visit user_path(user2)
-  end
-
-  scenario 'accept friend request' do
-    click_link 'Accept'
-    visit users_path
-    expect(page).to have_content('Friends!!!')
-  end
-
-  scenario 'decline friend request' do
-    click_link 'Decline'
-    visit users_path
-    expect(page).to have_content('Send friend request')
+  context 'Testing unfriend button' do 
+    let(:user) { User.create(id: '1', name: 'Peter', email: 'peter@example.com', password: 'password') }
+    before :each do
+      user2 = User.create(id: '2', name: 'Mick', email: 'mick@example.com', password: 'password')
+      visit new_user_session_path
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
+      click_button 'Log in'
+      visit users_path
+      click_link 'Send friend request'
+      click_link 'Sign out'
+      visit new_user_session_path
+      fill_in 'user_email', with: user2.email
+      fill_in 'user_password', with: user2.password
+      click_button 'Log in'
+      visit users_path
+      click_link 'Accept'
+      visit users_path
+    end
+    scenario 'should unfriend' do 
+      click_link 'Unfriend'
+      expect(page).to have_content('Send friend request')
+    end
   end
 end
